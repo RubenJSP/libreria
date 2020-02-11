69415a3a930aa580325360c7c8bc4bad7e7e1c18
RubenJSP <56006571+RubenJSP@users.noreply.github.com>
2020-02-10 17:30:14 -0700

    Initial commit

---
+++ database/migrations/2020_02_10_231236_create_libros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('idLibro');
+            $table->string('titulo');
+            $table->string('autor');
+            $table->integer('paginas');
+            $table->integer('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
